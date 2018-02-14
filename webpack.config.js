b7b940a0f26aa883785d0332ae9aa052fa212e3e
Bill Buchanan <bbuchanan@gmail.com>
2018-02-13 22:53:45 -0500

    01 Hello React commit, added a 'Hello' component using typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,90 @@
-let path = require('path');
-let webpack = require('webpack');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let path = require("path");
+let webpack = require("webpack");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let basePath = __dirname;
 
 module.exports = {
   context: path.join(basePath, "src"),
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"]
   },
 
-  entry: [
-    './main.ts',
-    '../node_modules/bootstrap/dist/css/bootstrap.css'
-  ],
+  entry: ["./main.tsx", "../node_modules/bootstrap/dist/css/bootstrap.css"],
   output: {
-    path: path.join(basePath, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(basePath, "dist"),
+    filename: "bundle.js"
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
 
   devServer: {
-    contentBase: './dist', // Content base
+    contentBase: "./dist", // Content base
     inline: true, // Enable watch and live reload
-    host: 'localhost',
+    host: "localhost",
     port: 8080,
-    stats: 'errors-only'
+    stats: "errors-only"
   },
 
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'awesome-typescript-loader',
+        loader: "awesome-typescript-loader",
         options: {
-          useBabel: true,
-        },
+          useBabel: true
+        }
       },
       {
         test: /\.css$/,
         include: /node_modules/,
         loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           use: {
-            loader: 'css-loader',
-          },
-        }),
+            loader: "css-loader"
+          }
+        })
       },
       // Loading glyphicons => https://github.com/gowravshekar/bootstrap-webpack
       // Using here url-loader and file-loader
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream"
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: 'assets/img/[name].[ext]?[hash]'
+          name: "assets/img/[name].[ext]?[hash]"
         }
-      },
+      }
     ]
   },
   plugins: [
     // Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html', // Name of file in ./dist/
-      template: 'index.html', // Name of template in ./src
+      filename: "index.html", // Name of file in ./dist/
+      template: "index.html", // Name of template in ./src
       hash: true
     }),
     new ExtractTextPlugin({
-      filename: '[chunkhash].[name].css',
+      filename: "[chunkhash].[name].css",
       disable: false,
-      allChunks: true,
-    }),
+      allChunks: true
+    })
   ]
-}
+};
