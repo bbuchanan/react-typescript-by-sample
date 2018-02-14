817b01b20d4ebdac77da58f80d8ed7621ea6b392
Bill Buchanan <bbuchanan@gmail.com>
2018-02-14 14:18:43 -0500

    06 revert to stateless, simply converted the name edit component back to a stateless component

--- src/nameEdit.tsx
+++ src/nameEdit.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { Fragment } from "react";
-import { Component } from "react";
 
 interface Props {
   editingUserName: string;
@@ -8,30 +6,19 @@ interface Props {
   onNameUpdateRequest: () => void;
 }
 
-export class NameEditComponent extends Component<Props, {}> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <div>
-        <label>Update Name:</label>
-        <input
-          value={this.props.editingUserName}
-          onChange={(e): void =>
-            this.props.onEditingNameUpdated(
-              (e.target as HTMLInputElement).value
-            )
-          }
-        />
-        <button
-          className="btn btn-default"
-          onClick={this.props.onNameUpdateRequest}
-        >
-          Change
-        </button>
-      </div>
-    );
-  }
-}
+export const NameEditComponent = (props: Props) => {
+  return (
+    <div>
+      <label>Update Name:</label>
+      <input
+        value={props.editingUserName}
+        onChange={(e): void =>
+          props.onEditingNameUpdated((e.target as HTMLInputElement).value)
+        }
+      />
+      <button className="btn btn-default" onClick={props.onNameUpdateRequest}>
+        Change
+      </button>
+    </div>
+  );
+};
